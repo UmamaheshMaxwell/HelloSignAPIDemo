01a0b22247cf1556c548cfd1f178d92672d20a48
UmamaheshMaxwell <maheshmeka16@gmail.com>
2016-04-02 18:45:39 +0530

    chnages folder names for zip

--- SignatureRequest/downloadSignatureRequest.js
+++ SignatureRequest/downloadSignatureRequest.js
@@ -3,8 +3,8 @@ var hellosign = require('hellosign-sdk')({key: config.HELLOSIGN_APIKEY});
 
 hellosign.signatureRequest.download('5da6373e7ac93935ecc3b239758d5ff6a4b45844', {file_type: 'zip'}, function(err, response){
     var fs = require('fs');
-    var file = fs.createWriteStream(__dirname + "/Public/" + "mySignedPDF");
-    console.log(file);
+    var file = fs.createWriteStream(__dirname + "/zipfiles/" + "allSignedPDFs");
+    console.log(file.path);
     response.pipe(file);
     file.on('finish', function(response) {
         file.close();        
